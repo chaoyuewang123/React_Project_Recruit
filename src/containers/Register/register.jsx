cbc4d985b26b21f85bfc0c43f08bef548b009c15
Chaoyue Wang <wangchaoyuebeck@gmail.com>
2023-04-03 20:02:49 -0700

    finish register page collection data function

--- src/containers/Register/register.jsx
+++ src/containers/Register/register.jsx
@@ -4,10 +4,48 @@ import Logo from '../../components/logo/logo'
 
 export default function Register() {
 
+
+  const [Username, setUsername] = React.useState("");
+  const [Password, setPassword] = React.useState("");
+  const [ConPassword, setConPassword] = React.useState("");
+  const [Identity, setIdentity] = React.useState('1');
+
   function register1(){
-    console.log(11)
+    if(Username === ''){
+      alert('Username cant be empty');
+      return;
+    }
+    else if(Password === ''){
+      alert('Password cant be empty');
+      return;
+    }
+    else if(ConPassword === ''){
+      alert('Password cant be empty');
+      return;
+    }
+    else{
+    const totalObj= {
+      Username,
+      Password,
+      ConPassword,
+      Identity
+    }
+    console.log(totalObj);
+  }
+  }
+  function handleUsername(value){
+      setUsername(value)
   }
 
+  function handlePassword(value){
+    setPassword(value)
+  }
+  function handleConPassword(value){
+    setConPassword(value)
+  }
+  function handleIdentity(value){
+    setIdentity(value)
+  }
   return (
     <div>
       <NavBar style={{ backgroundColor: '#1DA57A' }} back={null}>Recruit</NavBar>
@@ -16,16 +54,16 @@ export default function Register() {
       <Form layout='horizontal' mode='card' style={{border:'1px ridge', borderRadius:'12px',width:'90%',marginLeft:'5%'}}>
       <Form.Header />
         <Form.Item>
-         <Input placeholder='Username' />
+         <Input placeholder='Username' onChange={val=>{handleUsername(val)}} value={Username}/>
         </Form.Item>
         <Form.Item>
-          <Input placeholder='Password' clearable type='password'/>
+          <Input placeholder='Password' clearable type='password' onChange={val=>{handlePassword(val)}} value={Password}/>
         </Form.Item>
         <Form.Item>
-          <Input placeholder='Confirm Password' clearable type='password'/>
+          <Input placeholder='Confirm Password' clearable type='password' onChange={val=>{handleConPassword(val)}} value={ConPassword}/>
         </Form.Item>
         <Form.Item> 
-          <Radio.Group>
+          <Radio.Group  onChange={val=>{handleIdentity(val)}} value={Identity}>
             <Radio value='1'>Boos</Radio>&nbsp;
             <Radio value='2'>Candidates</Radio>
           </Radio.Group>
