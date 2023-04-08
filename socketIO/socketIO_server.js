8ced01a127de2c7a6c0566a6d14273e570415a40
Chaoyue Wang <wangchaoyuebeck@gmail.com>
2023-04-07 20:56:48 -0700

    finish message router

--- socketIO/socketIO_server.js
+++ socketIO/socketIO_server.js
@@ -1,22 +1,21 @@
-const {ChatModel} =require('../db/models')
+const { ChatModel } = require('../db/models')
 module.exports = function (server) {
-    // 得到 IO 对象
-    const io = require("socket.io")(server, { cors: true })
-    io.listen(4001)
-    io.on('connection', socket => {
-        console.log('有客户端连接成功了')
-      
-        // 监听客户端发送的消息
-        socket.on('sendMsg', ({from,to,content}) => {
-          console.log('服务器接收到消息:', {from,to,content})
-          const chat_id = [from,to].sort().join('_')
-          const creat_time =Date.now()
-          new ChatModel({from ,to , content, chat_id, creat_time}).save()
-          .then((chatMsg)=>{
-            io.emit('receiveMsg',chatMsg)
-          }).catch((err)=>{
-            console.log(err);
-          })
+
+  const io = require("socket.io")(server, { cors: true })
+  io.listen(4001)
+  io.on('connection', socket => {
+    console.log('有客户端连接成功了')
+
+    socket.on('sendMsg', ({ from, to, content }) => {
+      console.log('服务器接收到消息:', { from, to, content })
+      const chat_id = [from, to].sort().join('_')
+      const creat_time = Date.now()
+      new ChatModel({ from, to, content, chat_id, creat_time }).save()
+        .then((chatMsg) => {
+          io.emit('receiveMsg', chatMsg)
+        }).catch((err) => {
+          console.log(err);
         })
-      })
-    }
+    })
+  })
+}
